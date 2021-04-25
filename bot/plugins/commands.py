0da71641af25e076efe30804bf0c9d6d576fc442
Shamil Habeeb <shamilhabeeb5@gmail.com>
2021-04-25 10:06:00 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -23,7 +23,7 @@ async def start(bot, update):
         if (file_id or file_type) == None:
             return
         
-        caption = file_caption if file_caption != ("" or None) else ("<code>" + file_name + "</code>")
+        caption = file_caption if file_caption != ("" or None) else ("<code>" + file_name + "</code>\n<b>Uploaded By : @movieworldkdy</b>")
         
         if file_type == "document":
         
